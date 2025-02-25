4f55fd94e4520ccee2b0d7e172db69722e3fdbc3
kchrusciel <krzysztof.chrusciel@outlook.com>
2025-02-25 10:48:42 +0100

    feat: add logger

--- src/main/java/pl/codecouple/spock/flowdocs/FlowDocsSpecListener.java
+++ src/main/java/pl/codecouple/spock/flowdocs/FlowDocsSpecListener.java
@@ -1,17 +1,29 @@
 package pl.codecouple.spock.flowdocs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spockframework.runtime.AbstractRunListener;
 import org.spockframework.runtime.model.ErrorInfo;
+import org.spockframework.runtime.model.SpecInfo;
 
 /**
  * Flow docs spec listener generates documentation after each iteration.
  */
 public class FlowDocsSpecListener extends AbstractRunListener {
 
+    private static final Logger log = LoggerFactory.getLogger(FlowDocsSpecListener.class);
+
     private ErrorInfo errorInfo;
 
     @Override
     public void error(ErrorInfo error) {
         errorInfo = error;
+    }
+
+    @Override
+    public void afterSpec(SpecInfo specInfo) {
+        if (errorInfo != null) {
+            log.warn("FlowDocs generation is skipped. Spec failed.");
+        }
     }
 }
